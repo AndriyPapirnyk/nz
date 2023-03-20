ab83e4f02c17c73febee675e68c39f8b6b017e16
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-03-20 22:05:37 +0200

    wefwf

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,19 @@
+function sendData(formData, formId, method, methodName) {
+    const form = document.getElementById(`${formId}`);
+    fetch(`${methodName}`, {
+        method: `${method}`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(_=>{
+        form.reset();
+    })
+    .catch(error => console.log(error))
+}
+
+
 $('#SignUp__sumbitBtn').click(function(){
     let data = {
         name:signUp__inpName.value,
@@ -8,6 +24,35 @@ $('#SignUp__sumbitBtn').click(function(){
     };
     sendPurchaseData(data);
     window.location = "http://localhost:3000/signUp";
+})
+
+
+
+
+function sendPurchaseData(formData) {
+    const form = document.getElementById('SignUp__form');
+    fetch('signUp', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(_=>{
+        form.reset();
+    })
+    .catch(error => console.log(error))
+}
+
+
+
+$('#Login__button').click(function(){
+    let data = {
+        name:signUp__inpName.value,
+        surname:signUp__inpSurname.value,
+        password:signUp__inpPassword.value
+    };
+    sendPurchaseData(data);
 })
 
 
